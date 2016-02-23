7fef1aeece584796e06e7068fcb897ab4c4bcb99
puroc <471049931@qq.com>
2016-02-23 15:35:57 +0800

    Signed-off-by: puroc <471049931@qq.com>

---
+++ camel-disruptor-test/src/main/java/com/example/camel/disruptor/Main.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * @(#)Main.java 2016年2月23日
+ *
+ * Copyright 2016 emrubik Group Ltd. All rights reserved.
+ * emrubik PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *******************************************************************************/
+package com.example.camel.disruptor;
+
+/**
+ * @author <a href="mailto:pud@emrubik.com">pu dong</a>
+ * @version $Revision 1.0 $ 2016年2月23日 下午3:13:07
+ */
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
